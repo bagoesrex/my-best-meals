3a0502b05601a9aeb9b9003fcbfd58b14308f2ae
bagoesrex <bagoesrex@gmail.com>
2025-06-28 23:08:06 +0700

    feat: update Header component content and structure

--- app/page.js
+++ app/page.js
@@ -3,21 +3,23 @@ import Header from "../components/header";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen w-full">
+    <main className="h-screen w-full">
       <Header />
-      <h1 className="font-sans pt-2">My Fav Masakan Istri (Megumin)</h1>
-      <p className="text-orange-500 underline underline-offset-4">
-        <Link href={"/about"}>About Link</Link>
-      </p>
-      <p className="text-orange-500 underline underline-offset-4">
-        <Link href={"/meals"}>Meals Link</Link>
-      </p>
-      <p className="text-orange-500 underline underline-offset-4">
-        <Link href={"/meals/share"}>Share Meals Link</Link>
-      </p>
-      <p className="text-orange-500 underline underline-offset-4">
-        <Link href={"/community"}>Community Link</Link>
-      </p>
+      <div className="flex flex-col mt-2">
+        <h1 className="font-sans pt-2">My Fav Masakan Istri (Megumin)</h1>
+        <p className="text-orange-500 underline underline-offset-4">
+          <Link href={"/about"}>About Link</Link>
+        </p>
+        <p className="text-orange-500 underline underline-offset-4">
+          <Link href={"/meals"}>Meals Link</Link>
+        </p>
+        <p className="text-orange-500 underline underline-offset-4">
+          <Link href={"/meals/share"}>Share Meals Link</Link>
+        </p>
+        <p className="text-orange-500 underline underline-offset-4">
+          <Link href={"/community"}>Community Link</Link>
+        </p>
+      </div>
     </main>
   );
 }
--- components/header.js
+++ components/header.js
@@ -1,8 +1,37 @@
+import Link from "next/link";
+
 export default function Header() {
-    return (
-        <>
-            <img src="/logo.png" alt="Header logo" className="size-30 object-cover"/>
-            <h1 className="text-2xl font-extrabold font-mono">Selamat datang</h1>
-        </>
-    )
+  return (
+    <header className="flex items-center justify-between px-6 py-2 bg-white shadow-md">
+      <Link href="/" className="flex items-center space-x-2">
+        <img
+          src="/logo.png"
+          alt="Header logo"
+          className="w-12 h-12 object-cover"
+        />
+        <span className="text-xl font-bold text-gray-800">MY BEST MEALS</span>
+      </Link>
+
+      <nav>
+        <ul className="flex gap-6 text-gray-700 font-medium">
+          <li>
+            <Link
+              href="/meals"
+              className="hover:text-primary transition-colors duration-200"
+            >
+              Browse Meals
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/community"
+              className="hover:text-primary transition-colors duration-200"
+            >
+              Foodies Community
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
