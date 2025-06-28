68fe23acc19d961ba61d287fc422e29f6c996f12
bagoesrex <bagoesrex@gmail.com>
2025-06-28 22:30:49 +0700

    refactor: move reusable components outside app directory

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <>
-            <img src="/logo.png" alt="Header logo" className="size-30"/>
+            <img src="/logo.png" alt="Header logo" className="size-30 object-cover"/>
             <h1 className="text-2xl font-extrabold font-mono">Selamat datang</h1>
         </>
     )
