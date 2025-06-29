690dd9c66a010571f73b5881728f49821f20b969
bagoesrex <bagoesrex@gmail.com>
2025-06-29 22:55:43 +0700

    feat: implement meals fetching

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,4 +1,10 @@
-export default function MealsPage() {
+import MealsGrid from "@/components/meals-grid";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <main className="relative flex flex-col items-center justify-center w-full px-6 py-12 z-30 text-center">
       <header className="max-w-4xl w-full space-y-6">
@@ -8,15 +14,13 @@ export default function MealsPage() {
         <p className="text-lg text-gray-600">
           Choose your favorite recipe and cook it yourself. It's easy and fun!
         </p>
-        <p className="text-lg text-gray-600">
-          Share your favorite recipes and inspire others to try new dishes.
+        <p className="text-md text-white bg-orange-400 rounded-xl py-2 px-4 w-fit mx-auto border-2 border-white">
+          <Link href="/meals/share">Share your favorite recipes</Link>
         </p>
       </header>
 
       <main className="max-w-4xl w-full mt-16">
-        <p className="text-gray-500 text-center">
-            Todo Grid
-        </p>
+        <MealsGrid meals={meals} />
       </main>
     </main>
   );
