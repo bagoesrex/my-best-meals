94ece2f32653161025467c7997ec4b9ac8c32362
bagoesrex <bagoesrex@gmail.com>
2025-06-29 16:34:09 +0700

    feat: enhance Meals page with header and content

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,23 @@
 export default function MealsPage() {
-    return (
-        <h1>Meals Page</h1>
-    )
+  return (
+    <main className="relative flex flex-col items-center justify-center w-full px-6 py-12 z-30 text-center">
+      <header className="max-w-4xl w-full space-y-6">
+        <h1 className="text-4xl font-extrabold text-gray-800">
+          Delicious Meals, Created <span className="text-primary">by You</span>
+        </h1>
+        <p className="text-lg text-gray-600">
+          Choose your favorite recipe and cook it yourself. It's easy and fun!
+        </p>
+        <p className="text-lg text-gray-600">
+          Share your favorite recipes and inspire others to try new dishes.
+        </p>
+      </header>
+
+      <main className="max-w-4xl w-full mt-16">
+        <p className="text-gray-500 text-center">
+            Todo Grid
+        </p>
+      </main>
+    </main>
+  );
 }
