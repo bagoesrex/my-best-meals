69343f68071cd1972b1edd413cdf8f1b51bb8441
bagoesrex <bagoesrex@gmail.com>
2025-06-29 23:13:37 +0700

    feat: show loading indicator while fetching meals

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <main className="relative flex flex-col items-center justify-center w-full px-6 py-12 z-30 text-center">
+        <p className="text-center animate-pulse text-lg">Fetching Meals</p>
+    </main>
+  );
+}
